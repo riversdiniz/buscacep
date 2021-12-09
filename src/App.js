da187736a2abd367668259c3b7b369048f5d122f
River Diniz <river-s-diniz@hotmail.com>
2021-12-09 00:48:14 -0400

    styles.css
    Criando o styles.css e adicionado os novos codigos.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
+
+
 function App() {
   return (
     <div className="container">
@@ -8,7 +13,22 @@ function App() {
         placeholder="Digite seu cep..."
         />
 
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+
       </div>
+
+      <main className="main">
+        <h2>CEP: 69079250</h2>
+
+        <span>Rua Nunes Cardoso</span>
+        <span>Complemento: Algum complemento</span>
+        <span>São Francisco</span>
+        <span>Manaus - AM</span>
+
+      </main>
+
     </div>
   );
 }
