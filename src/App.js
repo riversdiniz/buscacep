ed91e3cd7dafea42341c9b0650d3dcf018af4cb8
River Diniz <river-s-diniz@hotmail.com>
2021-12-12 23:32:07 -0400

    Pasta services
    Criado a pasta services com e arquivo api, para busca cep.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
 
-
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('')
+
+  async function handleSearch(){
+    
+    if(input === ''){
+      alert("Preencha algum cep!")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      console.log(response)
+
+    }catch{
+      alert("Ops erro ao buscar");
+    }
+
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
@@ -11,9 +32,11 @@ function App() {
       <div className="containerInput">
         <input type="text"
         placeholder="Digite seu cep..."
+        value={input}
+        onChange={(e) => setInput(e.target.value) }
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF"/>
         </button>
 
